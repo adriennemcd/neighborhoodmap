ff575537fe6d2c48b90efdf50b357cd8dcba83be
Adrienne McDonnell <adrienne.mcd.21@gmail.com>
2015-07-20 20:45:58 -0400

    removed latlong alert, changed limit for posts on index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all.order("created_at DESC").limit(5)
+    @posts = Post.all.order("created_at DESC").limit(10)
     @posts2 = Post.all
     @hash = Gmaps4rails.build_markers(@posts2) do |post, marker|
       marker.lat post.latitude
